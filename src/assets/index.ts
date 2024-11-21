9f1b0993fdba7529448102b2ed4ea9985cb21f5a
Rahul Sutradhar <rahulsd380@gmail.com>
2024-11-21 09:56:11 +0600

    Set up project

---
+++ src/assets/index.ts
@@ -0,0 +1,14 @@
+// Import icons here
+
+
+
+// Import images here
+import heroBanner from "../assets/images/heroBanner.png"
+
+export const ICONS = {
+
+}
+
+export const IMAGES = {
+    heroBanner,
+}
