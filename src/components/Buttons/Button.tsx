5280e24e5daa11ce75c8587d89a7e29748ff2c24
Rahul Sutradhar <rahulsd380@gmail.com>
2024-11-23 22:25:28 +0600

    Added reusable button

--- src/components/Buttons/Button.tsx
+++ src/components/Buttons/Button.tsx
@@ -1,50 +1,18 @@
-import React from 'react';
+type TButton = {
+  variant: "Outlined" | "Filled";
+  label: string;
+};
 
-interface ButtonProps {
-  text: string;
-  onClick: () => void;
-  className?: string; 
-  type?: 'button' | 'submit' | 'reset';
-  disabled?: boolean;
-  image?: string; // Optional image URL or path
-  imageAlt?: string; // Alt text for the image
-  buttonColor?: string; // Background color for the button
-  textcolor?:string // Button Text color
-  horizontalpadding?:number
-  verticalpadding?:number
-  bordercolor?:string
-  isButtonOutlined?:boolean
-}
-
-const Button: React.FC<ButtonProps> = ({
-  text,
-  onClick,
-  className = '',
-  type = 'button',
-  disabled = false,
-  image,
-  imageAlt = 'button image',
-  // buttonColor = 'bg-gradient-to-b from-[#FF7F50]  to-[#FF7F50]', 
-  buttonColor='#FF7F50',
-  textcolor='#ffffff',
-  horizontalpadding=16,
-  verticalpadding=20,
-  isButtonOutlined=false
-}) => {
-    // background: linear-gradient(180deg, #FF7F50 0%, #F07F38 90.13%);
-const textColorClass=isButtonOutlined ?`text-[${buttonColor}]`: `text-[${textcolor}]`;
-const bgColorClass=`bg-[${buttonColor}]`;
-const borderColorClass=`bg-transparent border border-[${buttonColor}] `;
-
+const Button: React.FC<TButton> = ({ variant, label }) => {
   return (
     <button
-      type={type}
-      onClick={onClick}
-      className={`flex items-center justify-center  font-semibold  text-[22px] font-poppins rounded-full  ${textColorClass} px-${horizontalpadding}px py-${verticalpadding}px  ${isButtonOutlined? borderColorClass : bgColorClass}  ${className}`}
-      disabled={disabled}
+      className={`${
+        variant === "Outlined"
+          ? "border-[#FF7F50] bg-none text-[#FF7F50]"
+          : "border-[#FF7F50] bg-[#FF7F50] text-[#F5F5DC]"
+      } border px-11 py-4  font-Poppins text-[22px] font-semibold rounded-[55px]`}
     >
-      {image && <img src={image} alt={imageAlt} className="w-5 h-5 mr-2" />}
-      <span>{text}</span>
+      {label}
     </button>
   );
 };
--- src/components/Home/Hero/Hero.tsx
+++ src/components/Home/Hero/Hero.tsx
@@ -1,37 +1,24 @@
-"use client"
-import Button from '@/components/Buttons/Button';
-import React from 'react';
-
-
+"use client";
+import Button from "@/components/Buttons/Button";
+import React from "react";
 
 const Hero = () => {
-    const handleClick = () => {
-        alert("Button clicked!");
-      };
-    return (
-        <div>
-           <Button
-        text="Schedule an appointment"
-        // image="/assets/images/FooterCall.svg"
-        onClick={handleClick}
-        textcolor='#FFFFFF'
-        className="h-[76px] w-[401px]"
-        verticalpadding={202}
-        horizontalpadding={500}
+  // const handleClick = () => {
+  //   alert("Button clicked!");
+  // };
+  return (
+    <div>
+      <Button
+      variant="Outlined"
+      label="Book Now!"
       />
-      <Button  text="Book Now"
-        // image="/assets/images/FooterCall.svg"
-        isButtonOutlined={true}
-        onClick={handleClick}
-        // buttonColor="bg-transparent"
-        textcolor='#FF7F50'
-        verticalpadding ={16}
-        horizontalpadding={44}
-        buttonColor='#FF7F50'
-        className="h-[65px] w-[205px]  " />
-        
-        </div>
-    );
+      <Button
+      variant="Filled"
+      label="Sign In"
+      />
+      
+    </div>
+  );
 };
 
-export default Hero; 
+export default Hero;
